12e8c4531e916fe8e10c29490cfad28c48af6aa4
Steven.Dellamore <Steven.DA.Dellamore@partner.bmw.de>
2020-07-17 08:37:29 -0500

    stuff

---
+++ 11.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define rl(x) getline(cin, x); stringstream ss(x);
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll> > matrix;
+typedef pair<ll, ll> pll;
+
+// remember you may need to reset state
+void rsolve() {
+
+}
+
+void solve() {
+	ll n, a;
+	cin >> n;
+	while(n--){
+		cin >> a;
+		cout << (char)a;
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    solve();
+}
